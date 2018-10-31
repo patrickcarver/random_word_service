9be530822d9162649d8fd96f14fe8f40af135fe3
Patrick Carver <patrickcarver@gmail.com>
2018-10-30 21:16:51 -0500

    load verb file

--- lib/random_word_service.ex
+++ lib/random_word_service.ex
@@ -2,8 +2,26 @@ defmodule RandomWordService do
   @moduledoc """
   Provides a random word given part of speech, number, tense, etc.
   """
-  
+
+  alias Poison
+
   def get_word(acronym) do
-    acronym
+    load_json("../text_files/verbs.json")
+
+  end
+
+  defp load_text(file) do
+    file
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing/1) 
+    |> Enum.to_list()
+  end
+
+  defp load_json(file) do
+    file
+    |> Path.expand(__DIR__)
+    |> File.read!()
+    |> Poison.decode!()
   end
 end
