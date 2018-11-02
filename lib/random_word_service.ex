970c9d5ead70cbb9385ef8fbeca55ed4c4b6e737
Patrick Carver <patrickcarver@gmail.com>
2018-11-01 19:27:53 -0500

    convert verb list keys to atoms

--- lib/random_word_service.ex
+++ lib/random_word_service.ex
@@ -5,8 +5,11 @@ defmodule RandomWordService do
 
   alias Poison
 
+  @text_dir "../text_files/"
+
   def get_word(acronym) do
-    load_json("../text_files/verbs.json")
+
+    load_json(@text_dir <> "verbs.json")
 
   end
 
@@ -23,5 +26,9 @@ defmodule RandomWordService do
     |> Path.expand(__DIR__)
     |> File.read!()
     |> Poison.decode!()
+    |> Enum.map(fn m -> 
+          Enum.map(m, fn {key, value} -> {String.to_atom(key), value} end)
+          |> Enum.into(%{})
+       end)
   end
 end
