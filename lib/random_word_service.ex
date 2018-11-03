de1e8a8af5f857acb5aa52ba5e174e8dc06d4efb
Patrick Carver <patrickcarver@gmail.com>
2018-11-02 21:43:31 -0500

    ListLoader pattern matches on txt vs json on how to parse

--- lib/random_word_service.ex
+++ lib/random_word_service.ex
@@ -3,7 +3,7 @@ defmodule RandomWordService do
   Provides a random word given part of speech, number, tense, etc.
   """
 
-  alias RandomWordService.{NonverbListLoader, VerbListLoader}
+  alias RandomWordService.ListLoader
 
   @text_dir "../../text_files/"
   @name __MODULE__
@@ -14,35 +14,40 @@ defmodule RandomWordService do
     Agent.start_link(fn -> %@name{} end, name: @name)
   end
 
-  @doc """
-  ["nouns", "adjectives", "adverbs", "verbs"]  
-  """
-  def load_from_files(file_names) do
-    file_names
-    |> Stream.map(fn name -> Task.async(fn -> load_file(name) end) end)
-    |> Enum.map(&Task.await/1)
+  def load_from_files() do
+    do_load_from_files(["adjectives", "adverbs", "nouns", "verbs"])
   end
 
   def lists() do
     Agent.get(@name, fn struct -> struct end)
   end
 
+  defp do_load_from_files(file_names) do
+    file_names
+    |> Stream.map(fn name -> Task.async(fn -> load_file(name) end) end)
+    |> Enum.map(&Task.await/1)
+  end
+
   defp load_file(file_name) when file_name == "verbs" do
-    list = VerbListLoader.load_from_file(@text_dir <> file_name <> ".json")
-    add_list(file_name, list)
+    do_load_file(file_name, "json")
   end
 
   defp load_file(file_name) do
-    list = NonverbListLoader.load_from_file(@text_dir <> file_name <> ".txt")
-    add_list(file_name, list)
+    do_load_file(file_name, "txt")
   end
 
-  defp add_list(key, list) do
+  defp do_load_file(file_name, extension) do
+    %{path: @text_dir, name: file_name, extension: extension}
+    |> ListLoader.load_from_file
+    |> add_to_list_by_key(file_name)    
+  end
+
+  defp add_to_list_by_key(list, key) do
     Agent.update(@name, fn struct -> Map.put(struct, String.to_atom(key), list) end)
   end
 
   def get_word(acronym) do
-
+    acronym
 
 
   end
--- lib/random_word_service/list_loader.ex
+++ lib/random_word_service/list_loader.ex
@@ -5,28 +5,30 @@ defmodule RandomWordService.ListLoader do
 
   alias Poison
 
-  def load_from_file(%{file_name: name, extension: "json" } = file_info) do
-    file_info.name <> "." <> file_info.extension
+  def load_from_file(%{extension: "txt" } = file_info) do
+    file_info
+    |> get_full_file_name()
     |> Path.expand(__DIR__)
     |> File.stream!()
     |> Stream.map(&String.trim_trailing/1) 
     |> Enum.to_list()    
   end
 
-  def load_from_file(%{file_name: name, extension: "txt"} = file_info) do
-    file_info.name <> "." <> file_info.extension
+  def load_from_file(%{extension: "json"} = file_info) do
+    file_info
+    |> get_full_file_name()
     |> Path.expand(__DIR__)
     |> File.read!()
     |> Poison.decode!()
     |> Enum.map(&convert_string_keys_to_atoms/1)
   end
 
-  @doc """
-  Since the keys from the loaded json file are strings,
-  we convert them to atoms for cleaner querying
-  """
   defp convert_string_keys_to_atoms(map) do
     Enum.map(map, fn {key, value} -> {String.to_atom(key), value} end)
     |> Enum.into(%{})    
   end  
+
+  defp get_full_file_name(%{path: path, name: name, extension: extension}) do
+    path <> name <> "." <> extension 
+  end
 end
