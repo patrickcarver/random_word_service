35ac89dfc528db43336d82b6229fa368f0c16a2c
Patrick Carver <patrickcarver@gmail.com>
2018-11-03 22:40:57 -0500

    Move Dictionary code directly to RandomWordService

--- lib/random_word_service.ex
+++ lib/random_word_service.ex
@@ -1,16 +1,75 @@
 defmodule RandomWordService do
   @moduledoc """
-  Provides a random word given part of speech, number, tense, etc.
+  
   """
 
-  alias RandomWordService.Dictionary
+  @text_dir "../text_files/"
+  @parts_of_speech [:adjective, :adverb, :noun, :verb] 
+  @name __MODULE__
 
-  def start_link() do
-    
+  defstruct(adjectives: [], adverbs: [], nouns: [], verbs: [])
+  
+  def init() do
+    {:ok, pid} = start_link()
+    load_from_files()
+
+    {:ok, pid}
   end
 
-  def get_word(acronym) do
-    acronym
+  def get_random_word(starts_with: letter, part_of_speech: part_of_speech) 
+    when part_of_speech in @parts_of_speech do
+    word = part_of_speech
+           |> pluralize()
+           |> get_part_of_speech_list()
+           |> Enum.filter(fn word -> String.starts_with?(word, letter) end)
+           |> Enum.random()
+
+    {:ok, word}
   end
 
+  def get_random_word(starts_with: _letter, part_of_speech: invalid_part_of_speech) do
+    {:error, "Sorry, but #{invalid_part_of_speech} is not in parts of speech list."}
+  end
+
+  def lists() do
+    Agent.get(@name, fn struct -> struct end)
+  end
+
+
+
+  defp start_link() do
+    Agent.start_link(fn -> %@name{} end, name: @name)
+  end
+
+  defp load_from_files() do
+    do_load_from_files(["adjectives", "adverbs", "nouns", "verbs"])
+  end
+
+  defp get_part_of_speech_list(key) do
+    {:ok, list} = Agent.get(@name, fn struct -> Map.fetch(struct, key)  end)
+    list
+  end  
+
+  defp pluralize(part_of_speech) do
+    Atom.to_string(part_of_speech) <> "s" |> String.to_atom()  
+  end  
+
+  defp do_load_from_files(file_names) do
+    file_names
+    |> Stream.map(fn name -> Task.async(fn -> load_file(name) end) end)
+    |> Enum.map(&Task.await/1)
+  end
+
+  defp load_file(file_name) do
+    @text_dir <> file_name <> ".txt"
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing/1) 
+    |> Enum.to_list()
+    |> add_to_list_by_key(file_name)   
+  end
+
+  defp add_to_list_by_key(list, key) do
+    Agent.update(@name, fn struct -> Map.put(struct, String.to_atom(key), list) end)
+  end  
 end
