c6a6526faf5a675582dd8c9adc52da66596e1144
Patrick Carver <patrickcarver@gmail.com>
2018-11-12 17:26:46 -0600

    change parts_of_speech to include atom versions.

--- lib/random_word_service.ex
+++ lib/random_word_service.ex
@@ -3,7 +3,7 @@ defmodule RandomWordService do
   
   """
 
-  @parts_of_speech ["adjective", "adverb", "noun", "verb"] 
+  @parts_of_speech ["adjective", "adverb", "noun", "verb", :adjective, :adverb, :noun, :verb] 
   @name __MODULE__
 
   defstruct(adjective: [], adverb: [], noun: [], verb: [])
@@ -20,22 +20,21 @@ defmodule RandomWordService do
     { :error, "Cannot use an empty string in starts_with" }
   end
 
-  def get_random_word(starts_with: letter, part_of_speech: part_of_speech) do
-    acceptable_parts_of_speech = @parts_of_speech ++ Enum.map(@parts_of_speech, &String.to_atom/1)
+  def get_random_word(starts_with: letter, part_of_speech: part_of_speech) 
+    when part_of_speech in @parts_of_speech do
 
-    case part_of_speech in acceptable_parts_of_speech do
-      true ->
+    word = part_of_speech
+            |> atomize_part_of_speech()
+            |> get_part_of_speech_list()
+            |> Enum.filter(fn word -> String.starts_with?(word, letter) end)
+            |> Enum.random()
 
-        word = part_of_speech
-                |> atomize_part_of_speech()
-                |> get_part_of_speech_list()
-                |> Enum.filter(fn word -> String.starts_with?(word, letter) end)
-                |> Enum.random()
+    {:ok, word}
+  end
 
-        {:ok, word}
-      false ->
-        {:error, "Cannot find #{part_of_speech} in parts of speech list."}
-    end
+  def get_random_word(starts_with: _letter, part_of_speech: part_of_speech)
+    when part_of_speech not in @parts_of_speech do
+      {:error, "Cannot find #{part_of_speech} in parts of speech list."}
   end
 
   def get_random_word(_) do
@@ -46,7 +45,7 @@ defmodule RandomWordService do
     Agent.get(@name, fn struct -> struct end)
   end
 
-  def atomize_part_of_speech(part_of_speech) do
+  defp atomize_part_of_speech(part_of_speech) do
     case is_binary(part_of_speech) do
       true -> String.to_atom(part_of_speech)
       false -> part_of_speech
@@ -60,6 +59,7 @@ defmodule RandomWordService do
 
   defp load_from_files() do
     @parts_of_speech
+    |> Enum.filter(&is_binary/1)
     |> Stream.map(fn name -> Task.async(fn -> load_file(name) end) end)
     |> Enum.map(&Task.await/1)
     |> convert_to_struct()
