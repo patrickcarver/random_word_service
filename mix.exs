9116e9cb19a45ffb86cd10a47db1469e162f76f7
Patrick Carver <patrickcarver@gmail.com>
2018-11-12 17:06:00 -0600

    increment version in mix.exs to 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RandomWordService.MixProject do
   def project do
     [
       app: :random_word_service,
-      version: "0.1.0",
+      version: "0.3.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
