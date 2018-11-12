91fa77b2580715842c6eaa7cc3209dae70e82378
Patrick Carver <patrickcarver@gmail.com>
2018-11-12 17:03:32 -0600

    create tests and fix code to handle tests

--- lib/random_word_service.ex
+++ lib/random_word_service.ex
@@ -12,21 +12,45 @@ defmodule RandomWordService do
     Agent.start_link(&load_from_files/0, name: @name)
   end
 
-  def get_random_word(starts_with: letter, part_of_speech: part_of_speech) do
-    word = part_of_speech
-           |> get_part_of_speech_list()
-           |> Enum.filter(fn word -> String.starts_with?(word, letter) end)
-           |> Enum.random()
-
-    {:ok, word}
+  def get_random_word(starts_with: letter, part_of_speech: _) when not is_binary(letter) do
+    { :error, "Cannot use a non-string in starts_with" } 
   end
 
- # def get_random_word(starts_with: _letter, part_of_speech: invalid_part_of_speech) do
- #   {:error, "Sorry, but #{invalid_part_of_speech} is not in parts of speech list."}
- # end
+  def get_random_word(starts_with: "", part_of_speech: _) do
+    { :error, "Cannot use an empty string in starts_with" }
+  end
+
+  def get_random_word(starts_with: letter, part_of_speech: part_of_speech) do
+    acceptable_parts_of_speech = @parts_of_speech ++ Enum.map(@parts_of_speech, &String.to_atom/1)
+
+    case part_of_speech in acceptable_parts_of_speech do
+      true ->
+
+        word = part_of_speech
+                |> atomize_part_of_speech()
+                |> get_part_of_speech_list()
+                |> Enum.filter(fn word -> String.starts_with?(word, letter) end)
+                |> Enum.random()
+
+        {:ok, word}
+      false ->
+        {:error, "Cannot find #{part_of_speech} in parts of speech list."}
+    end
+  end
+
+  def get_random_word(_) do
+    { :error, "Cannot use invalid options" }
+  end
 
   def lists() do
     Agent.get(@name, fn struct -> struct end)
+  end
+
+  def atomize_part_of_speech(part_of_speech) do
+    case is_binary(part_of_speech) do
+      true -> String.to_atom(part_of_speech)
+      false -> part_of_speech
+    end
   end
 
   defp get_part_of_speech_list(key) do
--- test/random_word_service_test.exs
+++ test/random_word_service_test.exs
@@ -2,7 +2,48 @@ defmodule RandomWordServiceTest do
   use ExUnit.Case
   doctest RandomWordService
 
-  test "greets the world" do
-    assert RandomWordService.hello() == :world
+  test "get a random word starting with 'a' and an adjective" do
+    { :ok, word } = RandomWordService.get_random_word(starts_with: "a", part_of_speech: :adjective)
+    assert String.starts_with?(word, "a")
+  end
+  
+  test "starts_with is an integer" do
+    result = RandomWordService.get_random_word(starts_with: 1, part_of_speech: :adjective)
+    assert { :error, "Cannot use a non-string in starts_with" } == result
+  end
+
+  test "starts_with is an empty string" do
+    result = RandomWordService.get_random_word(starts_with: "", part_of_speech: :adjective) 
+    assert { :error, "Cannot use an empty string in starts_with" } == result   
+  end
+
+  test "part_of_speech is not valid" do
+    result = RandomWordService.get_random_word(starts_with: "a", part_of_speech: :adjctiv)
+    assert { :error, "Cannot find adjctiv in parts of speech list." } == result
+  end
+
+  test "part_of_speech is not used" do
+    result = RandomWordService.get_random_word(starts_with: "a")
+    assert { :error, "Cannot use invalid options" } == result
+  end
+
+  test "starts_with is not used" do
+    result = RandomWordService.get_random_word(part_of_speech: :adjective)
+    assert { :error, "Cannot use invalid options" } == result
+  end
+
+  test "invalid keyword stars_with passed in along with valid keyword part_of_speech" do
+    result = RandomWordService.get_random_word(stars_with: "a", part_of_speech: :noun)
+    assert { :error, "Cannot use invalid options" } == result
+  end
+
+  test "part_of_speech can be a string in valid parts of speech" do
+    result = RandomWordService.get_random_word(starts_with: "a", part_of_speech: "noun")
+    assert { :ok, word } = result
+  end
+
+  test "part_of_speech can be an atom in valid parts of speech" do
+    result = RandomWordService.get_random_word(starts_with: "a", part_of_speech: :noun)
+    assert { :ok, word } = result    
   end
 end
