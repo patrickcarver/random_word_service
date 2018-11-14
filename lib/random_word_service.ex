f33e9b207f90e9bbe6018e182a1630d86d1dbb4e
Patrick Carver <patrickcarver@gmail.com>
2018-11-14 03:03:26 -0600

    Remove error checking in prep of redo

--- lib/random_word_service.ex
+++ lib/random_word_service.ex
@@ -3,7 +3,7 @@ defmodule RandomWordService do
   
   """
 
-  @parts_of_speech ["adjective", "adverb", "noun", "verb", :adjective, :adverb, :noun, :verb] 
+  @parts_of_speech [:adjective, :adverb, :noun, :verb] 
   @name __MODULE__
 
   defstruct(adjective: [], adverb: [], noun: [], verb: [])
@@ -12,29 +12,14 @@ defmodule RandomWordService do
     Agent.start_link(&load_from_files/0, name: @name)
   end
 
-  def get_random_word(starts_with: letter, part_of_speech: _) when not is_binary(letter) do
-    { :error, "Cannot use a non-string in starts_with" } 
-  end
-
-  def get_random_word(starts_with: "", part_of_speech: _) do
-    { :error, "Cannot use an empty string in starts_with" }
-  end
-
-  def get_random_word(starts_with: letter, part_of_speech: part_of_speech) 
-    when part_of_speech in @parts_of_speech do
+  def get_random_word(starts_with: letter, part_of_speech: part_of_speech) do
 
     word = part_of_speech
-            |> atomize_part_of_speech()
             |> get_part_of_speech_list()
             |> Enum.filter(fn word -> String.starts_with?(word, letter) end)
             |> Enum.random()
 
     {:ok, word}
-  end
-
-  def get_random_word(starts_with: _letter, part_of_speech: part_of_speech)
-    when part_of_speech not in @parts_of_speech do
-      {:error, "Cannot find #{part_of_speech} in parts of speech list."}
   end
 
   def get_random_word(_) do
@@ -43,13 +28,6 @@ defmodule RandomWordService do
 
   def lists() do
     Agent.get(@name, fn struct -> struct end)
-  end
-
-  defp atomize_part_of_speech(part_of_speech) do
-    case is_binary(part_of_speech) do
-      true -> String.to_atom(part_of_speech)
-      false -> part_of_speech
-    end
   end
 
   defp get_part_of_speech_list(key) do
@@ -78,6 +56,6 @@ defmodule RandomWordService do
       |> Stream.map(&String.trim_trailing/1) 
       |> Enum.to_list()
 
-    { String.to_atom(file_name), list }  
+    { file_name, list }  
   end
 end
