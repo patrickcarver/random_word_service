6804ab49cb92b6732211ad024b4bd7c470ad99d7
Patrick Carver <patrickcarver@gmail.com>
2018-11-15 23:20:17 -0600

    Create tests for StartsWith.validate

---
+++ test/random_word_service/validators/part_of_speech_test.exs
@@ -0,0 +1,4 @@
+defmodule RandomWordService.Validators.PartOfSpeechTest do
+  use ExUnit.Case
+  doctest RandomWordService.Validators.StartsWith
+end
---
+++ test/random_word_service/validators/starts_with_test.exs
@@ -0,0 +1,24 @@
+defmodule RandomWordService.Validators.StartsWithTest do
+  use ExUnit.Case
+  doctest RandomWordService.Validators.StartsWith
+
+  alias RandomWordService.Validators.StartsWith
+
+  test "starts_with is a string but with numbers in it" do
+    error = { :error, "starts_with must contain only English alphabetic characters" } 
+    assert error == StartsWith.validate("a1")
+  end
+
+  test "starts_with is an integer" do
+    error = { :error, "starts_with must be a string" }
+    assert error == StartsWith.validate(1) 
+  end
+
+  test "starts_with is a lower case string" do
+    assert {:ok, "a" } == StartsWith.validate("a")
+  end
+
+  test "starts_with is an upper case string" do
+    assert {:ok, "a" } == StartsWith.validate("A")
+  end
+end
