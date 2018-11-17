82daec8debdd6a566672cbda7e4914522f937187
Patrick Carver <patrickcarver@gmail.com>
2018-11-17 10:35:38 -0600

    change test from using == to =

--- test/random_word_service/validators/starts_with_test.exs
+++ test/random_word_service/validators/starts_with_test.exs
@@ -6,19 +6,19 @@ defmodule RandomWordService.Validators.StartsWithTest do
 
   test "starts_with is a string but with numbers in it" do
     error = { :error, "starts_with must contain only English alphabetic characters" } 
-    assert error == StartsWith.validate("a1")
+    assert error = StartsWith.validate("a1")
   end
 
   test "starts_with is an integer" do
     error = { :error, "starts_with must be a string" }
-    assert error == StartsWith.validate(1) 
+    assert error = StartsWith.validate(1) 
   end
 
   test "starts_with is a lower case string" do
-    assert {:ok, "a" } == StartsWith.validate("a")
+    assert {:ok, "a" } = StartsWith.validate("a")
   end
 
   test "starts_with is an upper case string" do
-    assert {:ok, "a" } == StartsWith.validate("A")
+    assert {:ok, "a" } = StartsWith.validate("A")
   end
 end
