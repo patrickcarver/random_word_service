622801198b3e9f2474cc1410d9af8021a0dad8e4
Patrick Carver <patrickcarver@gmail.com>
2018-11-18 13:07:21 -0600

    Re-add supervisor to application

--- lib/random_word_service/application.ex
+++ lib/random_word_service/application.ex
@@ -2,6 +2,18 @@ defmodule RandomWordService.Application do
   use Application
 
   def start(_type, _args) do
-    RandomWordService.start_link()
+
+    import Supervisor.Spec
+
+    children = [
+      worker(RandomWordService, [])
+    ]
+
+    options = [
+      name: RandomWordService.Supervisor,
+      strategy: :one_for_one
+    ]
+
+    Supervisor.start_link(children, options)
   end
 end
